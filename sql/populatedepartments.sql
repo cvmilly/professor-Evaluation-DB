0c6f31bd7c2d94adaedb7ca7b41d764229600366
cvmilly <43281584+cvmilly@users.noreply.github.com>
2021-12-10 16:55:48 -0500

    Create populatedepartments.sql

---
+++ sql/populatedepartments.sql
@@ -0,0 +1,224 @@
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0101, 01, 'Aerospace Studies (Air Force ROTC)');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0101, 02, 'Anthropology');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0101, 03, 'Biological Science');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0101, 04, 'Chemistry & Biochemistry');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0101, 05, 'Classics');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0101, 06, 'Computer Science');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0101, 07, 'Earth, Ocean & Atmospheric Science');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0101, 08, 'English');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0101, 09, 'History');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0101, 10, 'Mathematics');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0101, 11, 'Military Science (Army ROTC)');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0101, 12, 'Modern Languages & Linguistics');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0101, 13, 'Philosophy');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0101, 14, 'Physics');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0101, 15, 'Psychology');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0101, 16, 'Religion');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0101, 17, 'Scientific Computing');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0101, 18, 'Statistics');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0102, 01, 'Accounting');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0102, 02, 'Business Analytics, Information Systems & Supply Chain');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0102, 03, 'Finance');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0102, 04, 'Management');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0102, 05, 'Marketing');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0102, 06, 'Risk Management/Insurance, Real Estate & Legal Studies');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0103, 01, 'Communication');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0103, 02, 'Communication Science and Disorders');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0103, 03, 'Information');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0104, 01, 'Criminology & Criminal Justice');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0105, 01, 'Distance Learning');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0105, 02, 'Educational Leadership & Policy Studies');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0105, 03, 'Educational Psychology & Learning Systems');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0105, 04, 'Teacher Education');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0105, 05, 'Sport Management');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0106, 01, 'Chemical & Biomedical Engineering');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0106, 02, 'Civil & Environmental Engineering');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0106, 03, 'Electrical & Computer Engineering');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0106, 04, 'Industrial & Manufacturing Engineering');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0106, 05, 'Mechanical Engineering');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0107, 01, 'Art');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0107, 02, 'Art Education');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0107, 03, 'Art History');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0107, 04, 'Dance');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0107, 05, 'Interior Architecture and Design');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0107, 06, 'Theatre');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0108, 01, 'Family & Child Sciences');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0108, 02, 'Nutrition, Food & Exercise Sciences');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0109, 01, 'Law');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0110, 01, 'Medicine');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0111, 01, 'Motion Picture Arts');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0112, 01, 'Arts Administration');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0112, 02, 'Bands');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0112, 03, 'Brass and Woodwinds');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0112, 04, 'Choral');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0112, 05, 'Composition');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0112, 06, 'Ethnomusicology/World Music');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0112, 07, 'Historical Musicology');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0112, 08, 'Jazz Studies');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0112, 09, 'Keyboard');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0112, 10, 'Music Education');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0112, 11, 'Music Theatre');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0112, 12, 'Music Therapy');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0112, 13, 'Music Theory');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0112, 14, 'Opera Production');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0112, 15, 'Percussion');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0112, 16, 'Strings/Orchestra');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0112, 17, 'Voice');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0113, 01, 'Nursing');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0114, 01, 'Economics');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0114, 02, 'Geography');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0114, 03, 'Political Science');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0114, 04, 'Public Administration & Policy');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0114, 05, 'Sociology');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0114, 06, 'Urban & Regional Planning');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0115, 01, 'Social Work');
+
+INSERT INTO Department (college_id, dept_id, dept_name)
+VALUES (0100, 01, 'Applied Sciences');
