aacf9a791d5ffb8d372eae7c0d3fc8b6745f4c97
cvmilly <43281584+cvmilly@users.noreply.github.com>
2021-12-10 16:55:12 -0500

    Create populatecourses.sql

---
+++ sql/populatecourses.sql
@@ -0,0 +1,113 @@
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('MAC1105', 'College Algebra', 3, FALSE, 0010, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('MAC114', 'Trigonometry', 2, FALSE, 0010, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('MAC2311', 'Calculus I', 4, FALSE, 0010, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('MAC1140', 'Precalculus', 3, FALSE, 0010, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('MAD2104', 'Discrete Math I', 3, TRUE, 0010, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('MAD3105', 'Discrete Math II', 3, TRUE, 0010, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('MAC2312', 'Calculus II', 4, FALSE, 0010, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('STA4442', 'Intro to Probability', 3, FALSE, 0010, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('STA1013', 'Statistics through Example', 3, TRUE, 0010, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('STA2023', 'Fundamental Business Statistics', 3, TRUE, 0010, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('STA2122', 'Introduction to Applied Statistics', 3, TRUE, 0010, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('COP3252', 'Internet Applications Programming with Java', 3, TRUE, 0006, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('COP3330', 'Object Oriented Programming', 3, TRUE, 0006, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('COP3353', 'Introduction to UNIX', 1, TRUE, 0006, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('COP4020', 'Programming Languages', 3, TRUE, 0006, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('COP4530', 'Data Structures, Algorithms, and Generic Programming', 3, TRUE, 0006, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('COP4610', 'Introduction to Operating Systems', 3, TRUE, 0006, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('COP3014', 'Programming I', 3, TRUE, 0006, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('COT4401', 'Top 10 Algorithms', 3, TRUE, 0006, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('COT4420', 'Theory of Computation', 3, TRUE, 0006, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('CDA3100', 'Computer Organization I', 3, TRUE, 0006, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('CDA3101', 'Computer Organization II', 3, TRUE, 0006, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('CIS3250', 'Ethics and Computer Science', 3, TRUE, 0006, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('CIS4360', 'Introduction to Computer Security', 3, TRUE, 0006, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('CEN4020', 'Software Engineering', 3, TRUE, 0006, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('CEN4021', 'Software Engineering II', 3, TRUE, 0006, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('PHY2048', 'General Physics I', 4, FALSE, 0014, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('PHY2049', 'General Physics II', 4, FALSE, 0014, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('PHY2048L', 'General Physics I Lab', 1, FALSE, 0014, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('PHY2049L', 'General Physics II Lab', 1, FALSE, 0014, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('CHM1045', 'General Chemistry I', 3, FALSE, 0004, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('CHM1045L', 'General Chemistry I Lab', 1, FALSE, 0004, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('CHM1046', 'General Chemistry II', 3, FALSE, 0004, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('CHM1046L', 'General Chemistry II Lab', 1, FALSE, 0004, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('BSC2010', 'Biological Science I', 3, FALSE, 0003, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('BSC2010L', 'Biological Science I Lab', 1, FALSE, 0003, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('BSC2011', 'Biological Science II', 3, FALSE, 0003, 0101);
+
+INSERT INTO Course (course_id, course_name, credits, dist_learning, dept_id, college_id)
+VALUES ('BSC2011L', 'Biological Science II Lab', 1, FALSE, 0003, 0101);
