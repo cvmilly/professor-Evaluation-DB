d8ed6d583fd2768e7dc006e183f4888aaadb9836
cvmilly <43281584+cvmilly@users.noreply.github.com>
2021-12-10 16:54:36 -0500

    Create populatecolleges.sql

---
+++ sql/populatecolleges.sql
@@ -0,0 +1,47 @@
+INSERT INTO College (college_id, college_name)
+VALUES (0100, 'Applied Studies');
+
+INSERT INTO College (college_id, college_name)
+VALUES (0101, 'Arts and Sciences');
+
+INSERT INTO College (college_id, college_name)
+VALUES (0102, 'Business');
+
+INSERT INTO College (college_id, college_name)
+VALUES (0103, 'Communication and Information');
+
+INSERT INTO College (college_id, college_name)
+VALUES (0104, 'Criminology and Criminal Justice');
+
+INSERT INTO College (college_id, college_name)
+VALUES (0105, 'Education');
+
+INSERT INTO College (college_id, college_name)
+VALUES (0106, 'Engineering');
+
+INSERT INTO College (college_id, college_name)
+VALUES (0107, 'Fine Arts');
+
+INSERT INTO College (college_id, college_name)
+VALUES (0108, 'Human Sciences');
+
+INSERT INTO College (college_id, college_name)
+VALUES (0109, 'Law');
+
+INSERT INTO College (college_id, college_name)
+VALUES (0110, 'Medicine');
+
+INSERT INTO College (college_id, college_name)
+VALUES (0111, 'Motion Picture Arts');
+
+INSERT INTO College (college_id, college_name)
+VALUES (0112, 'Music');
+
+INSERT INTO College (college_id, college_name)
+VALUES (0113, 'Nursing');
+
+INSERT INTO College (college_id, college_name)
+VALUES (0114, 'Social Sciences and Public Policy');
+
+INSERT INTO College (college_id, college_name)
+VALUES (0115, 'Social Work'); 
