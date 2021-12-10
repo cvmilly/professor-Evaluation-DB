4083b5a17678c02160023f11ffd66e2f28ecba5c
cvmilly <43281584+cvmilly@users.noreply.github.com>
2021-12-10 16:56:18 -0500

    Create populate professors.sql

---
+++ sql/populate professors.sql
@@ -0,0 +1,262 @@
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES ( 1, "SUDHIR AGGARWAL", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES ( 2, "THEODORE P. BAKER", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES ( 3, "MIKE BURMESTER", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES ( 4, "SHAYOK CHARKRABORTY", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES ( 5, "ZHENHAI DUAN", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES ( 6, "ROBERT A. VAN ENGELEN", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (7, "DAVID GAITROS", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (8, "SONIA HAIDUC", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (9, "LOIS WRIGHT HAWKES", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (10, "VIET TUNG HOANG", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (11, "SHARANYA JAYARAMAN", 101, 6, "GRAD");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (12, "PLYUSH KUMAR", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (13, "RANDOLPH LANGLEY", 101, 6, NULL);
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (14, "HILBERT LEVITZ", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (15, "XIUWEN LIU", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (16, "MICHAEL MASCAGNI", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (17, "BOB MYERS", 101, 6, "M.S");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (18, "MELINA MYERS (Vastola)", 101, 6, NULL);
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (19, "ADRIAN NISTOR", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (20, "DANIEL SCHWARTZ", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (21, "ASHOK SRINIVASAN", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (22, "GARY TYSON", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (23, "AN-I ANDY WANG", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (24, "YU WANG", 101, 6, NULL);
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (25, "ZHI WANG", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (26, "DAVID WHALLEY", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (27, "JIE YANG", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (28, "WELKUAN YU", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (29, "XIN YUAN", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (30, "JIAWEL ZHANG", 101, 6, NULL);
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (31, "ZHENGHAO ZHANG", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (32, "PEIXIANG ZHAO", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (33, "R. CHRISTOPHER LACHER", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (34, "STEPHEN LEACH", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (35, "GANG-RYUNG UH", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (36, "H. SARP ORAL", 101, 6, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (37, "JACK WINSTON", 101, 6, "M.S");
+
+
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (38, "NEIL ABELL", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (39, "AMY AI", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (40, "NORMAN ANDERSON", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (41, "MARGARET ASHMORE", 115, 1, "M.S");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (42, "SHAMRA BOEL-STUDT", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (43, "KATRINA BOONE", 115, 1, "M.S");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (44, "CAROL CAMPBELL EDWARDS", 115, 1, "M.S");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (45, "HENRY CARRETTA", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (46, "JAMES CLARK", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (47, "ROSALYN DECKERHOFF", 115, 1, "M.S");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (48, "JANE DWYER LEE", 115, 1, "M.S");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (49, "GERRI GOLDMAN", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (50, "FRAN GOMORY", 115, 1, "M.S");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (51, "TOMI GOMORY", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (52, "PAM GRAHAM MACDILL", 115, 1, "M.S");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (53, "SHALAY JACKSON", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (54, "DONNA KELLEY", 115, 1, "M.S");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (55, "WILLIAM KINTZ", 115, 1, "M.S");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (56, "JEFFREY LACASSE", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (57, "MACHELLE MADSEN THOMPSON", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (58, "JOHN MATHIAS", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (59, "KEITHEN MATHIS", 115, 1, "M.S.W");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (60, "NICHOLAS MAZZA", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (61, "JEAN MUNN", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (62, "LA TONYA NOEL", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (63, "KAREN OEHME", 115, 1, "J.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (64, "SUSAN OSBORNE", 115, 1, "M.S.W");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (65, "CARRIE PETTUS-DAVIS", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (66, "ELLEN PIEKALKIEWICZ", 115, 1, "M.A");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (67, "JESSICA PRYCE", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (68, "MELISSA RADEY", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (69, "KAREN RANDOLPH", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (70, "TANYA RENN", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (71, "SHARON ROSS-DONALDSON", 115, 1, "M.B.A");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (72, "LISA SCHELBE", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (73, "THOMAS SMITH", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (74, "CRAIG STANLEY", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (75, "BRUCE THYER", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (76, "STEPHEN TRIPODI", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (77, "SHAIRI TURNER", 115, 1, "M.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (78, "VICTORIA VERANO", 115, 1, "M.S");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (79, "LINDA VINTON", 115, 1, "PH.D");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (80, "DINA WILKE", 115, 1, "PH.D");
+
+
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (81, "MARC VAN WERT", 101, 1, "COLONEL");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (82, "SCOTT POHLER", 101, 1, "LT COLONEL");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (83, "MICHAEL ANDERSON", 101, 1, "MAJOR");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (84, "DAVID MILLER", 101, 1, "CAPTAIN");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (85, "BRYANT BARR", 101, 1, "STAFF SERGEANT");
+
+INSERT INTO Professor (prof_id, prof_name, college_id, dept_id, credentials)
+VALUES (86, "RYAN HOLLAND", 101, 1, "STAFF SERGEANT");
+
